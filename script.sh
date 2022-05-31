55cf3f821c3b46665b25e4e15a95427f0886e6b2
evgeniaqa <e_balusheva@mail.ru>
2022-05-31 12:04:14 +0300

    add the files with hw1

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd lesson_1
+mkdir gitbash JS SQL
+cd gitbash
+touch theory.txt commands.txt homework.txt test1.json test2.json
+mkdir video screen posts
+ls -la
+mv {test1.json,test2.json} screen
